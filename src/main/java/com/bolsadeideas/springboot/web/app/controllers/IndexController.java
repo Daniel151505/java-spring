7e71baa9480c0a79388d2c83e193e7d3de391f9c
Daniel151505 <daniel150520011@gmail.com>
2023-09-03 23:02:57 -0500

    feat: added model object

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -5,13 +5,28 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.bolsadeideas.springboot.web.app.models.Usuario;
+
 @Controller
 @RequestMapping("/app")
 public class IndexController {
 	
-	@GetMapping({"/index", "/", "/home"})
+	@GetMapping({"/index", "/", "", "/home"})
 	public String index(Model model) {
 		model.addAttribute("titulo", "Hola Spring Framework");
 		return "index";
 	}
+	
+	@GetMapping("/perfil")
+	public String perfil(Model model) {
+		Usuario usuario = new Usuario();
+		usuario.setNombre("Andrés");
+		usuario.setApellido("Solis");
+		
+		model.addAttribute("usuario", usuario);
+		model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
+		
+		return "perfil";
+	}
+	
 }
