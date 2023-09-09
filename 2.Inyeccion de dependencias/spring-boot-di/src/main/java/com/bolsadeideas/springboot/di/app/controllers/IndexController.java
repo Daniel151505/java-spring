157aed3c3ec1a66a6175a5d701cf1c8ce14fb200
Daniel151505 <daniel150520011@gmail.com>
2023-09-08 23:27:36 -0500

    (2) - Inyección de dependencias vía constructor y métodos setters

--- 2.Inyeccion de dependencias/spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/controllers/IndexController.java
+++ 2.Inyeccion de dependencias/spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/controllers/IndexController.java
@@ -7,18 +7,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import com.bolsadeideas.springboot.di.app.models.service.IServicio;
 
-
-
 @Controller
 public class IndexController {
-	
+
 	@Autowired
 	private IServicio servicio;
 
-	@GetMapping({"/", "", "/index"}) 
-	public String index(Model model){
+	// @Autowired
+	// public IndexController(IServicio servicio) {
+	// this.servicio = servicio;
+	// }
+
+	// @Autowired
+	// public void setServicio(IServicio servicio) {
+	// this.servicio = servicio;
+	// }
+
+	@GetMapping({ "/", "", "/index" })
+	public String index(Model model) {
 		model.addAttribute("objeto", servicio.operacion());
 		return "index";
 	}
-	
+
 }
