4d89261006ac6f64d97427f93051914b089ebd68
Daniel151505 <daniel150520011@gmail.com>
2023-09-08 22:30:56 -0500

    (2) - Creando Controlador

--- 2.Inyeccion de dependencias/spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/controllers/IndexController.java
+++ 2.Inyeccion de dependencias/spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/controllers/IndexController.java
@@ -1,5 +1,16 @@
 package com.bolsadeideas.springboot.di.app.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import ch.qos.logback.core.model.Model;
+
+@Controller
 public class IndexController {
 
+	@GetMapping({"/", "", "/index"}) 
+	public String index(Model model){
+		return "index";
+	}
+	
 }
