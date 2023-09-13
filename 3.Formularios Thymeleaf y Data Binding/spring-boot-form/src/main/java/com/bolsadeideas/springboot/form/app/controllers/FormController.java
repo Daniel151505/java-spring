19c8677fd41465cf713d5e99dd21d127d4a9305a
Daniel151505 <daniel150520011@gmail.com>
2023-09-13 16:47:00 -0500

    (3) - Añadiendo componente service de Pais

--- 3.Formularios Thymeleaf y Data Binding/spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ 3.Formularios Thymeleaf y Data Binding/spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.support.SessionStatus;
 import com.bolsadeideas.springboot.form.app.editors.NombreMayusculaEditors;
 import com.bolsadeideas.springboot.form.app.models.domain.Pais;
 import com.bolsadeideas.springboot.form.app.models.domain.Usuario;
+import com.bolsadeideas.springboot.form.app.services.PaisService;
 import com.bolsadeideas.springboot.form.app.validation.UsuarioValidador;
 
 import jakarta.validation.Valid;
@@ -33,6 +34,9 @@ public class FormController {
 	
 	@Autowired
 	private UsuarioValidador validador;
+	
+	@Autowired
+	private PaisService paisService;
 	
 	@InitBinder
 	public void initBinder(WebDataBinder binder) {
@@ -46,13 +50,7 @@ public class FormController {
 	
 	@ModelAttribute("listaPaises")
 	public List<Pais> listaPaises() {
-		return Arrays.asList(
-				new Pais(1, "ES", "España"),
-				new Pais(2, "MX", "México"), 
-				new Pais(3, "CL", "Chile"), 
-				new Pais(4, "AR", "Argentina"), 
-				new Pais(5, "PE", "Perú"), 
-				new Pais(6, "CO", "Colombia"));
+		return paisService.listar();
 	}
 	
 	@ModelAttribute("paises")
