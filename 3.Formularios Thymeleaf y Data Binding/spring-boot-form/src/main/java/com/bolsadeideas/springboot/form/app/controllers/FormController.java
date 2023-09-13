b3ed826971c4e3a654ebfec7dba6ad1faef7baf7
Daniel151505 <daniel150520011@gmail.com>
2023-09-12 23:08:41 -0500

    (3) - Llenando lista select con Map

--- 3.Formularios Thymeleaf y Data Binding/spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ 3.Formularios Thymeleaf y Data Binding/spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -3,7 +3,9 @@ package com.bolsadeideas.springboot.form.app.controllers;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
@@ -44,6 +46,16 @@ public class FormController {
 	@ModelAttribute("paises")
 	public List<String> paises() {
 		return Arrays.asList("España", "México", "Chile", "Argentina", "Perú", "Colombia");
+	}
+	
+	@ModelAttribute("paisesMap")
+	public Map<String, String> paisesMap() {
+		Map<String, String> paises = new HashMap<String, String>();
+		paises.put("ES", "España");
+		paises.put("MX", "México");
+		paises.put("CL", "Chile");
+		return paises;
+		
 	}
 
 	@GetMapping("/form")
