06a3710f6a389d2288c8fe3bc14150b6de28bd4b
Daniel151505 <daniel150520011@gmail.com>
2023-09-17 19:23:10 -0500

    (5) - Revisando error NullPointerException

--- 5.Interceptores HTTP - Horario de acceso a clientes/spring-boot-horario/src/main/java/com/bolsadeideas/springboot/horariointerceptor/app/interceptors/HorarioInterceptor.java
+++ 5.Interceptores HTTP - Horario de acceso a clientes/spring-boot-horario/src/main/java/com/bolsadeideas/springboot/horariointerceptor/app/interceptors/HorarioInterceptor.java
@@ -4,6 +4,7 @@ import java.util.Calendar;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,17 +16,17 @@ public class HorarioInterceptor implements HandlerInterceptor {
 
 	@Value("${config.horario.apertura}")
 	private Integer apertura;
-	
+
 	@Value("${config.horario.cierre}")
 	private Integer cierre;
-	
+
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		
-		Calendar calendar= Calendar.getInstance();
+
+		Calendar calendar = Calendar.getInstance();
 		int hora = calendar.get(Calendar.HOUR_OF_DAY);
-		
+
 		if (hora >= apertura && hora < cierre) {
 			StringBuilder mensaje = new StringBuilder("Bienvenido l horario de atención a clientes");
 			mensaje.append(", atendemos desde las ");
@@ -38,7 +39,7 @@ public class HorarioInterceptor implements HandlerInterceptor {
 			request.setAttribute("mensaje", mensaje.toString());
 			return true;
 		}
-		
+
 		response.sendRedirect(request.getContextPath().concat("/cerrado"));
 		return false;
 	}
@@ -46,10 +47,13 @@ public class HorarioInterceptor implements HandlerInterceptor {
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
-		
+
 		String mensaje = (String) request.getAttribute("mensaje");
-		modelAndView.addObject("horario", mensaje);
-		
+
+		if (modelAndView != null && handler instanceof HandlerMethod) {
+			modelAndView.addObject("horario", mensaje);
+		}
+
 	}
 
 }
