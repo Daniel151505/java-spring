a64a9a7fc3e8470cac0e8a0a71ec8c198c615c80
Daniel151505 <daniel150520011@gmail.com>
2023-09-24 19:39:46 -0500

    (7) - Agregando crear en Repositorio Dao y Controlador

--- 7.Spring MVC y Data JPA/spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ 7.Spring MVC y Data JPA/spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -1,11 +1,16 @@
 package com.bolsadeideas.springboot.app.controllers;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import com.bolsadeideas.springboot.app.models.dao.IClienteDao;
+import com.bolsadeideas.springboot.app.models.entity.Cliente;
 
 @Controller
 public class ClienteController {
@@ -19,6 +24,20 @@ public class ClienteController {
 		model.addAttribute("titulo", "Listado de clientes");
 		model.addAttribute("clientes", clienteDao.findAll());
 		return "listar";
+	}
+	
+	@GetMapping(value="/form")
+	public String crear(Map <String, Object> model) {
+		Cliente cliente = new Cliente();
+		model.put("cliente", cliente);
+		model.put("titulo", "Formulario de Cliente");
+		return "form";
+	}
+	
+	@PostMapping(value="/form")
+	public String guardar(Cliente cliente) {
+		clienteDao.save(cliente);
+		return "redirect:listar";
 	}
 	
 }
