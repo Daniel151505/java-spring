fd88964b56a307c3322e30162ae1626f69e1086f
Daniel151505 <daniel150520011@gmail.com>
2023-09-27 23:34:56 -0500

    (8) - Paginador desde cero usando PagingAndSortingRepository, parte I

--- 7.Spring MVC y Data JPA/spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ 7.Spring MVC y Data JPA/spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -3,12 +3,15 @@ package com.bolsadeideas.springboot.app.controllers;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -26,9 +29,11 @@ public class ClienteController {
 	private IClienteService clienteService;
 	
 	@GetMapping("/listar")
-	public String Listar(Model model) {
+	public String Listar(@RequestParam(name = "page", defaultValue="0") int page, Model model) {
+		Page<Cliente> clientes = clienteService.findAll(PageRequest.of(page, 5));
+
 		model.addAttribute("titulo", "Listado de clientes");
-		model.addAttribute("clientes", clienteService.findAll());
+		model.addAttribute("clientes", clientes);
 		return "listar";
 	}
 	
