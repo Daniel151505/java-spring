4cafa23acd98dc281a4ed99be03a271a7a3c56b5
Daniel151505 <daniel150520011@gmail.com>
2023-09-27 23:17:47 -0500

    (8) - Flash messenger

--- 7.Spring MVC y Data JPA/spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ 7.Spring MVC y Data JPA/spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
 import com.bolsadeideas.springboot.app.models.service.IClienteService;
@@ -40,13 +41,18 @@ public class ClienteController {
 	}
 	
 	@GetMapping(value="/form/{id}")
-	public String editar(@PathVariable(value="id") Long id, Map <String, Object> model, SessionStatus status) {
+	public String editar(@PathVariable(value="id") Long id, Map <String, Object> model, RedirectAttributes flash, SessionStatus status) {
 	
 		Cliente cliente = null;
 		
 		if (id > 0) {
 			cliente = clienteService.findOne(id);
+			if (cliente == null) {
+				flash.addFlashAttribute("error", "El ID del cliente no existe en la BBDD!");
+				return "redirect:/listar";
+			}
 		} else {
+			flash.addFlashAttribute("error", "El ID del cliente no puede ser cero");
 			return "redirect:/listar";
 		}
 		model.put("cliente", cliente);
@@ -55,22 +61,26 @@ public class ClienteController {
 	}
 	
 	@PostMapping(value="/form")
-	public String guardar(@Valid Cliente cliente, BindingResult result, Model model, SessionStatus status) {
+	public String guardar(@Valid Cliente cliente, BindingResult result, Model model, RedirectAttributes flash, SessionStatus status) {
 		
 		if(result.hasErrors()) {
 			model.addAttribute("titulo", "Formulario de cliente");
 			return "form";
 		}
 		
+		String messageFlash = (cliente.getId() != null) ? "Cliente editado con éxito!" : "Cliente creado con éxito!";
+		
 		clienteService.save(cliente);
 		status.setComplete();
+		flash.addFlashAttribute("success", messageFlash);
 		return "redirect:listar";
 	}
 	
 	@GetMapping(value="/eliminar/{id}")
-	public String eliminar(@PathVariable(value="id") Long id) {
+	public String eliminar(@PathVariable(value="id") Long id, RedirectAttributes flash) {
 		if (id > 0) {
 			clienteService.delete(id);
+			flash.addFlashAttribute("success", "Cliente eliminado con éxito");
 		} 
 		return "redirect:/listar";
 	}
